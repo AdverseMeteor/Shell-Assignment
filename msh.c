aaf8e57b818c6d6471ccbc32a12c2b5bade4a1ea
AdverseMeteor <60937311+AdverseMeteor@users.noreply.github.com>
2020-10-02 18:10:29 -0500

    Update msh.c

--- msh.c
+++ msh.c
@@ -115,11 +115,6 @@ int main()
       break;
     }
 
-    else if(strcmp(token[0],"cd")==0)
-    {
-      chdir(token[1]);
-    }
-
     pid_t pid = fork( );
     if( pid == 0 )
     {
@@ -136,6 +131,11 @@ int main()
     {
       int status;
       wait( & status );
+
+      if(strcmp(token[0],"cd")==0)
+      {
+        chdir(token[1]);
+      }
 
       fflush(NULL);
     }
