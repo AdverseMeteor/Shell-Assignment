5c0caa8de342c93fd9c3b1421f516750c1be4c90
AdverseMeteor <60937311+AdverseMeteor@users.noreply.github.com>
2020-10-02 14:29:47 -0500

    Update msh.c

--- msh.c
+++ msh.c
@@ -87,6 +87,12 @@ int main()
 
     }
 
+    int token_index  = 0;
+    for( token_index = 0; token_index < token_count; token_index ++ )
+    {
+      printf("token[%d] = %s\n", token_index, token[token_index] );
+    }
+    
     free( working_root );
 
   }
