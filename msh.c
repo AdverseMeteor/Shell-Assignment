54515d144a813eafee5c908372951ac72acd5cd6
AdverseMeteor <60937311+AdverseMeteor@users.noreply.github.com>
2020-10-03 16:39:11 -0500

    Update msh.c

--- msh.c
+++ msh.c
@@ -26,6 +26,9 @@ int main()
 
   char * cmd_str = (char*) malloc( MAX_COMMAND_SIZE );
 
+  char * history[15];//Malloc and zero it out
+  int h_index;
+
   while( 1 )
   {
     // Print out the msh prompt
@@ -50,6 +53,7 @@ int main()
     char *working_str  = strdup( cmd_str );
 
     //Save History here
+    strncpy(history[h_index++], cmd_str, MAX_COMMAND_SIZE);
     if(cmd_str[0]=='!')
     {
 
